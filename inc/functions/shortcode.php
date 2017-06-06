be0671263f20f645d85c393394bf2d42aecc41da
Punal Chotrani <punalsc@gmail.com>
2017-06-06 14:19:40 +0100

    css fixes

--- inc/functions/shortcode.php
+++ inc/functions/shortcode.php
@@ -5,9 +5,9 @@ function a_z_shortcode(){
 	echo '<ul id="a-z-menu">';
 		foreach (range('A', 'Z') as $letter)
 		{
-			echo "<li>$letter</li>";
+			echo "<li><a>$letter</a></li>";
 		}
-		echo "<li>0-9</li>";
+		echo "<li><a>0-9</a></li>";
 	echo "</ul>";
 	echo "<div class='clearfix'></div>";
 }
